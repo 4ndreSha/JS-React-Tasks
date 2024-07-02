91b71b8755378f17c5117a6e4ced0061cf0d3878
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-07-02 14:14:20 +0300

    feat: added Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,23 @@
-import ReactDOM from "react-dom";
-import {App} from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {App} from './App';
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import Task1Page from './pages/task1';
+import Task2Page from './pages/task2';
+import Task3Page from './pages/task3';
+import Home from './pages/Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/task1' element={<Task1Page />} />
+        <Route path='/task2' element={<Task2Page />} />
+        <Route path='/task3' element={<Task3Page />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
